ba0088f9c484ff2520efde654fa4bc2be5e14a13
saradonin <m.jemieljanczuk@gmail.com>
2023-03-28 16:38:45 +0200

    Initial commit

---
+++ main.py
@@ -0,0 +1,23 @@
+from random import randint
+
+
+def guess_the_number():
+    """
+    """
+    answer = randint(1, 101)
+    print("Guess the number from 1 to 100: ")
+
+    while True:
+        try:
+            number = float(input())
+            if number == answer:
+                return "You win!"
+            elif number > answer:
+                print(f"Too big!")
+            else:
+                print(f"Too small!")
+        except ValueError:
+            print("It's not a number!")
+
+
+print(guess_the_number())
